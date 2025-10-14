a5f5704e8b0d453313742946bbe9143e29ea4398
Adriano <adricatena@gmail.com>
2025-10-14 11:11:33 -0300

    feat: expand environment variable exports in config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,11 @@
-export const { PAYLOAD_SECRET = '', DATABASE_URI = '' } = process.env
+export const {
+  PAYLOAD_SECRET = '',
+  DATABASE_URI = '',
+  NODE_ENV,
+  R2_ENDPOINT = '',
+  R2_ACCESS_KEY_ID = '',
+  R2_SECRET_ACCESS_KEY = '',
+  R2_BUCKET = '',
+} = process.env
+
+export const HIDE_API_URL = NODE_ENV === 'production'
